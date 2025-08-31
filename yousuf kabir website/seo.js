ce341fd14b4ba7061471ccd0d0e4fa3bdd3b2d06
shakib75alhasam-code <shakib75alhasam@gmail.com>
2025-08-31 20:52:18 +0600

    Update seo.js

--- yousuf kabir website/seo.js
+++ yousuf kabir website/seo.js
@@ -6,20 +6,21 @@
   const ld = {
     "@context": "https://schema.org",
     "@type": "Physician",
-    "name": "Dr. [DR_NAME]",
-    "description": "[SHORT_BIO]",
-    "url": "https://yourdomain.vercel.app/",
+    "name": "Dr. Md. Yousuf Kabir",
+    "description": "Thoracic Surgeon in Bangladesh, Lung Surgeon Dhaka, Chest Surgery Specialist, Empyema and Pleural Disease, VATS Surgery, Dr. Md. Yousuf Kabir,  Yousuf Kabir Dhaka Medical, Best Thoracic Doctor in Bangladesh"",
+    "url": "https://dryousufkabir.vercel.app/",
     "medicalSpecialty": "Thoracic Surgery",
-    "image": "https://yourdomain.vercel.app/assets/images/doctor-portrait.jpg",
-    "telephone": "+8801XXXXXXXXX",
+    "image": "https://dryousufkabir.vercel.app/assets/images/doctor-portrait.jpg",
+    "telephone": "+8801830140001",
     "address": {
       "@type": "PostalAddress",
-      "streetAddress": "[Chamber Address Line]",
+      "streetAddress": "152/2/G, Panthapath, Green Road, Dhaka-1205",
       "addressLocality": "Dhaka",
       "addressCountry": "BD"
     },
     "availableService": [
       {"@type":"MedicalProcedure","name":"Thoracic Surgery"},
+       "@type":"MedicalProcedure","name":"Broncities"},
       {"@type":"MedicalProcedure","name":"Lung Cancer Surgery"},
       {"@type":"MedicalProcedure","name":"Chest Trauma Management"}
     ],
@@ -35,3 +36,4 @@
   script.text = JSON.stringify(ld, null, 2);
   document.head.appendChild(script);
 })();
+
